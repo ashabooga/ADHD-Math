8a55356dbc0654978748d2f29e287494ec940816
Ben Cuello-Wolffe <bcuellowolffe@gmail.com>
2024-04-16 16:45:43 +0100

    Initial Commit

---
+++ ADHD Math/ADHD_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHD_MathApp.swift
+//  ADHD Math
+//
+//  Created by Cuello-Wolffe, Ben on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ADHD_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
