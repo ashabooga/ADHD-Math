e50415e73c8e98de0c747aaeefb2242b039ba05b
Ben Cuello-Wolffe <bcuellowolffe@gmail.com>
2024-04-25 18:36:48 +0100

    workinng on hints

---
+++ ADHD Math/Views/QuestionView/QuestionDisplayedHintsView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionDisplayedHintsView.swift
+//  ADHD Math
+//
+//  Created by Cuello-Wolffe, Ben on 25/04/2024.
+//
+
+import SwiftUI
+
+struct QuestionDisplayedHintsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionDisplayedHintsView()
+}
